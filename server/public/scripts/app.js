76812bc0990df0933112a7a726847dc293a2d7e5
Amanda Bausch <manadab@me.com>
2015-11-21 14:18:44 -0600

    draw functionality working

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
+  //sets the index value of the student in the object array
+  var currentlyDisplayedStudent = 0;
+  drawStudent();
 
-  $.ajax({url:'/data/eta.json'}).done(function(json) {
+  //getting json data from the server and bonding that data to the template
+  function drawStudent() {
+    $.ajax({url:'/data/eta.json'}).done(function(json) {
+    //id of the template, grabs the id and sets the source, a block of
+    //html with placeholders
     var source = $('#currentStudent').html();
+
+    //turns this block of html into a usable handlebars template
     var template = Handlebars.compile(source);
-    var context = {eta:[{firstName: json, lastName: json}]};
-    var html = template(context);
+
+    //takes the json data and putting it all into the template
+    var html = template(json.eta[currentlyDisplayedStudent]);
+
+    //appends the paired data and template to the dom in 'section'
     $('section').html(html);
   });
+  }
 });
